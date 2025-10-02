168b4f3214b6234693cf27c5bda43e04ece9ae4f
Firzen475 <Firzen475@gmail.com>
2025-10-02 15:04:35 +0300

    Fix: entrypoint.sh required_vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,20 +13,24 @@ done
 
 # --- Установка Ansible-коллекции ---
 echo "Устанавливаем Ansible-коллекцию из $ANSIBLE_COLLECTION_URL"
-#ansible-galaxy collection install "$ANSIBLE_COLLECTION_URL"
+ansible-galaxy collection install "$ANSIBLE_COLLECTION_URL"
 
 # --- Скачиваем inventory и structure, если URL задан ---
 INVENTORY_PATH=""
 STRUCTURE_PATH=""
 
 [ -n "$INVENTORY_URL" ] && {
+    echo "$INVENTORY_URL"
     curl -k -o /tmp/inventory.json "$INVENTORY_URL"
     INVENTORY_PATH="/tmp/inventory.json"
+    cat /tmp/inventory.json
 }
 
 [ -n "$STRUCTURE_URL" ] && {
+    echo "$STRUCTURE_URL"
     curl -k -o /tmp/structure.yaml "$STRUCTURE_URL"
     STRUCTURE_PATH="/tmp/structure.yaml"
+    cat /tmp/structure.yaml
 }
 
 # --- Формируем параметры -i только для существующих файлов ---
@@ -40,6 +44,9 @@ cat > ./playbook.yaml <<EOF
 - name: Import a playbook
   ansible.builtin.import_playbook: ${COLLECTION_PLAYBOOK}
 EOF
+
+
+
 
 # --- Запуск плейбука ---
 ansible-playbook ./playbook.yaml "$INVENTORY_PARAMS"
